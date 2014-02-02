3a61624bbf8c64465dbce466e97ab222fc618888
Boris Gribovskiy <xinu@yandex.ru>
2014-02-02 16:11:09 +0400

    complete sources

--- app/code/local/Boangri/Complex/sql/complex_setup/install-0.0.1.php
+++ app/code/local/Boangri/Complex/sql/complex_setup/install-0.0.1.php
@@ -12,12 +12,50 @@
 
 $installer = $this;
 $installer->startSetup();
-$installer->addEntityType('complex_eavblogpost', array(
-    //entity_model is the URI you'd pass into a Mage::getModel() call
+
+echo "installer start <br>";
+
+$installer->addEntityType('complex/eavblogpost', array(
+    //entity_mode is the URI you'd pass into a Mage::getModel() call
     'entity_model'    => 'complex/eavblogpost',
 
-    //table refers to the resource URI complex/eavblogpost
-    //<complex_resource>...<eavblogpost><table>eavblog_posts</table>
+    //table refers to the resource URI complexworld/eavblogpost
+    //<complexworld_resource>...<eavblogpost><table>eavblog_posts</table>
     'table'           =>'complex/eavblogpost',
 ));
-$installer->endSetup();
+echo "installer 1 <br>";
+
+//createEntityTables will automatically setup the tables we need, 
+//        as well as add some configuration rows to the system.
+$installer->createEntityTables(
+    $this->getTable('complex/eavblogpost')
+); 
+echo "installer 2 <br>";
+$this->addAttribute('complex_eavblogpost', 'title', array(
+        //the EAV attribute type, NOT a MySQL varchar
+        'type'              => 'varchar',
+        'label'             => 'Title',
+        'input'             => 'text',
+        'class'             => '',
+        'backend'           => '',
+        'frontend'          => '',
+        'source'            => '',
+        'required'          => true,
+        'user_defined'      => true,
+        'default'           => '',
+        'unique'            => false,
+    ));
+    $this->addAttribute('complex_eavblogpost', 'content', array(
+        'type'              => 'text',
+        'label'             => 'Content',
+        'input'             => 'textarea',
+    ));
+    $this->addAttribute('complex_eavblogpost', 'date', array(
+        'type'              => 'datetime',
+        'label'             => 'Post Date',
+        'input'             => 'datetime',
+        'required'          => false,
+    ));
+echo "installer end <br>";
+ 
+$installer->endSetup(); 
